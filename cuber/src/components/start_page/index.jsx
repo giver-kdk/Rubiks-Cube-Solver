8281b113a5e014760352d3894a799323cc34cdb4
Revig <45360605+giver-kdk@users.noreply.github.com>
2023-01-19 15:15:13 +0545

    UI Update

--- cuber/src/components/start_page/index.jsx
+++ cuber/src/components/start_page/index.jsx
@@ -10,7 +10,6 @@ export default function Start(props)
 		<div className="start__container flex__center--col">
 			<div className="start__heading">solve rubik's cube.</div>
 			<div className="cube__img--container">
-				{/* <img src={cube_start} alt="Isometric Rubiks Cube" className="cube__intro--pic" /> */}
 				<CubeRotate/>
 			</div>
 			<button className="start__btn" onClick={props.handleClick}>Start</button>
